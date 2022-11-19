56fc5079cbcb40b729e313f433e2c548c4294950
cheony <syc0704@gmail.com>
2022-11-19 23:00:20 +0900

    설명 추가

---
+++ MyBackjoon/MyBackjoon/1629.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+long long divide_conq(int a, int b, int c);
+
+int main()
+{
+	int a, b, c;
+	int i;
+	int result = 1;
+
+	cin >> a >> b >> c;
+
+	result = divide_conq(a, b, c);	
+	
+	int d = result % c;
+
+	printf("%d", d);
+	return 0;
+}
+
+long long divide_conq(int a, int b, int c)
+{
+//	if (b == 1)
+//		return a;
+
+	if (b == 0)
+		return 1;
+
+	long long result = divide_conq(a, b / 2, c);
+	result = result * result % c; // The result can be over integer range, the type of result should be long long..
+	if (b % 2)
+		result = result * a % c;
+
+	return result;
+}
